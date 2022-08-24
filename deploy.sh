713922af981138aa00bddc0a358b437248c77834
your potato <744863490@qq.com>
2022-08-24 16:48:20 +0800

    dd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+# set -e
 
 # 生成静态文件
 npm run build
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:Littlekid-cup/Littlekid-cup.github.io.git master
+git push -f git@github.com:Littlekid-cup/Littlekid-cup.github.io.git main
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
